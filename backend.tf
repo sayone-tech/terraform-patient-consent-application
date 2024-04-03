c229d0fca827a4a70da1faffdf233c6f748bfeb2
SayOne Technologies <50700068+sayone-tech@users.noreply.github.com>
2024-04-03 12:19:35 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket                  = "devopskid-terraform-backend"
-    key                     = "ec2-sg-ecr-sm-dev/terraform.tfstate"
-    region                  = "ap-southeast-1"
-    shared_credentials_file = "~/.aws/credentials"
-    profile                 = "my-terraform-admin"
-  }
-}
